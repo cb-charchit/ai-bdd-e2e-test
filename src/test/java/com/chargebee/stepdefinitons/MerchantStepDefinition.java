8424ebf196eb83dc82f00f3ff2b9c260415bdc39
cb-charchit <charchit.patodi@chargebee.com>
2022-08-22 23:08:42 +0530

    Rectified Then-section for MerchantStepDefinition

--- src/test/java/com/chargebee/stepdefinitons/MerchantStepDefinition.java
+++ src/test/java/com/chargebee/stepdefinitons/MerchantStepDefinition.java
@@ -1,24 +1,19 @@
 package com.chargebee.stepdefinitons;
 
-import com.factory.MerchantFactory;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import net.serenitybdd.rest.Ensure;
-import net.serenitybdd.screenplay.Actor;
-import net.serenitybdd.screenplay.actors.OnStage;
-import org.junit.Before;
 
-import static net.serenitybdd.rest.SerenityRest.given;
-import static net.serenitybdd.rest.SerenityRest.lastResponse;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MerchantStepDefinition {
+
+    private Response response;
 
     @Given("^(?:.*) is the merchant")
     public void actor_is_the_merchant() {
@@ -30,22 +25,22 @@ public class MerchantStepDefinition {
         String username = "test_VaPtEoC3nsnYJNRaFP0puyWWwMqPUPTs";
         String password = "test_VaPtEoC3nsnYJNRaFP0puyWWwMqPUPTs";
 
-        RestAssured.given().contentType(ContentType.JSON)
+        response = RestAssured.given().contentType(ContentType.JSON)
                 .auth().basic(username, password)
                 .log()
                 .all()
                 .basePath("/third_party_configurations")
                 .baseUri("https://neha-singla-test.chargebee.com/api/v2")
                 .queryParam("integration_name", integ_name.toString())
-                .when().get().then().extract();
+                .when().get().then().extract().response();
     }
 
 
     @Then("^(?:.*) should able to fetch Integration Configuration")
     public void actor_should_able_to_fetch_integration_configuration() {
-        //String qb_company_name = lastResponse().jsonPath().getString("third_party_configuration.config_json.qb_company_name");
-       // Ensure.that("Company name is Chargebee Inc", company_name -> company_name.equals(qb_company_name));
-        //assertThat(qb_company_name).isEqualTo("Chargebee Inc");
-        assertThat("Chargebee Inc").isEqualTo("Chargebee Inc");
+        String qb_company_name = response.jsonPath().getString("third_party_configuration.config_json.qb_company_name");
+        Ensure.that("Company name is Chargebee Inc", company_name -> qb_company_name.equals(company_name));
+        assertThat(qb_company_name).isEqualTo("Chargebee Inc");
+        //assertThat("Chargebee Inc").isEqualTo("Chargebee Inc");
     }
 }
